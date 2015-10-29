178b3cb89225a7e58b0a807fe6b9163af09eb700
Taku Kobayashi <taku.kobayashi@gamepot.co.jp>
2015-10-29 18:11:51 +0900

    Bluetooth発見可能ダイアログの追加

--- mobile/src/main/java/com/battle7/mbs/battle7/BluetoothActivity.java
+++ mobile/src/main/java/com/battle7/mbs/battle7/BluetoothActivity.java
@@ -1,18 +1,44 @@
 package com.battle7.mbs.battle7;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 public class BluetoothActivity extends AppCompatActivity {
 
+    private BluetoothAdapter mBluetoothAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bluetooth_view);
-        Button button = (Button) findViewById(R.id.button);
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        Button serach = (Button) findViewById(R.id.searchDevicebutton);
+        serach.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
+        Button scan = (Button) findViewById(R.id.scanDeviceButton);
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+                startActivity(discoverableIntent);
+            }
+        });
+        if (mBluetoothAdapter.getScanMode() != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+            scan.setVisibility(View.VISIBLE);
+        }else{
+            scan.setVisibility(View.INVISIBLE);
+        }
     }
 
     @Override
