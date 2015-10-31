ee406fe484e2ee73d328d9516962500ad8e60a11
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-10-31 21:00:24 +0900

    SprashView繋ぎこみ

---
+++ mobile/src/main/java/com/battle7/mbs/battle7/SprashActivity.java
@@ -0,0 +1,80 @@
+package com.battle7.mbs.battle7;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.ImageView;
+
+public class SprashActivity extends AppCompatActivity {
+    private static final int START_SCREEN_DISPLAY_TIME = 1000; // Millisecond
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(ExtraLayout.getParenetView(this, R.layout.sprash_view));
+
+        ImageView image = (ImageView) findViewById(R.id.sprashImage);
+        image.setImageResource(R.mipmap.gundom_bg);
+
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        bluetoothAdapter.enable();
+        scesuleNextActivity();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    private void scesuleNextActivity(){
+        Handler handler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+                //次のactivityを実行
+                Intent intent = new Intent(SprashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+            }
+        });
+        handler.sendEmptyMessageDelayed(0, START_SCREEN_DISPLAY_TIME);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ApplicationHelper.releaseImageView((ImageView) findViewById(R.id.sprashImage));
+    }
+}
